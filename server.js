e9554b8930cc2e86adfa3275111fd8bb408e5dff
Julie Carra <julie.carra@live.fr>
2020-06-12 15:35:07 +0200

    make it better

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
-const dbConfig = require("./config/dev");
+//const dbConfig = require("./config/dev");
 
 mongoose
-  .connect(dbConfig.MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
-  .then(x => {
+  .then((x) => {
     console.log(
       `Connected to MongoDB ! Database name: "${x.connections[0].name}"`
     );
   })
-  .catch(error => {
+  .catch((error) => {
     console.error("Error while connecting to MongoDB !", error);
   });
 
@@ -40,6 +41,13 @@ app.use("/api/reviews", require("./routes/reviews"));
 app.use("/api/addresses", require("./routes/addresses"));
 app.use("/api/payments", require("./routes/payments"));
 app.use("/api/orders", require("./routes/orders"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on: http://localhost:${process.env.PORT} !`);
